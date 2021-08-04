6fd2c73e3f8b4e0902d3225450229f0fbfbf7fd8
Evgenii Kosmogradskii-Ivanov <kosmogradsky@gmail.com>
2021-08-04 23:04:27 +0200

    changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,54 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import io from 'socket.io-client';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'razormafia-frontend3';
+
+  ngOnInit() {
+    const socket = io('ws://localhost:3000/auth', {
+      transports: ['websocket'],
+    });
+
+    const queueSocket = io('ws://localhost:3000/queue', {
+      transports: ['websocket'],
+    });
+
+    // socket.emit(
+    //   'register',
+    //   { email: 'kosmogradsky@gmail.com', password: '123456' },
+    //   (response: string) => {
+    //     console.log(response);
+    //   }
+    // );
+
+    // socket.emit(
+    //   'sign in with email and password',
+    //   { email: 'kosmogradsky@gmail.com', password: '123456' },
+    //   (response: string) => {
+    //     console.log(response);
+    //   }
+    // );
+
+    const token = "eyJhbGciOiJIUzI1NiJ9.UEpGSWh2R0lnNnI4eGZaT0dFWkZK.ebrNz7Gcp1wgqyZ0cJPgIV4n12sywQ_fFARNf8OBI3s";
+
+    socket.emit(
+      'sign in with session token',
+      token,
+      (response: string) => {
+        console.log(response);
+
+        // socket.emit(
+        //   'sign out',
+        //   (response: string) => {
+        //     console.log(response);
+        //   }
+        // );
+      }
+    );
+  }
 }
